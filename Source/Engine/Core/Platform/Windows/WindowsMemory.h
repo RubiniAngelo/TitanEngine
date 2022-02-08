2923a475cd04bd295a8a8dfd622a6013e7269246
RubiniAngelo <rubiniangelo04@hotmail.com>
2022-02-08 21:50:33 +0100

    Update

--- Source/Engine/Core/Platform/Windows/WindowsMemory.h
+++ Source/Engine/Core/Platform/Windows/WindowsMemory.h
@@ -3,20 +3,22 @@
 #include "Base/BasePlatformMemory.h"
 #include "Core/Math/Math.h"
 
-class TITAN_API WindowsMemory final //: public BasePlatformMemory
+#include <malloc.h>
+
+class TITAN_API WindowsMemory final : public BasePlatformMemory
 {
 public:
     /** Allocates memory on a specified alignment boundary */
     FORCEINLINE static void* Allocate(const Size size, const Size alignment)
     {
-        // ASSERT(Math::IsPowerOfTwo(alignment), "The alignment must be a power of two!");
+        ASSERT(Math::IsPowerOfTwo(alignment), "The alignment must be a power of two!");
         return _aligned_malloc(size, alignment);
     }
 
     /** Frees a block of memory that was allocated */
     FORCEINLINE static void Free(void* ptr)
     {
-        // ASSERT(ptr != nullptr, "The pointer is nullptr!");
+        ASSERT(ptr != nullptr, "The pointer is nullptr!");
         return _aligned_free(ptr);
     }
 };
