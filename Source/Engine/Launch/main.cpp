629c37e16a120cb9e65708b5958eacfccfd37fed
RubiniAngelo <99084249+RubiniAngelo@users.noreply.github.com>
2022-02-10 09:29:16 +0100

    Update main.cpp

--- Source/Engine/Launch/main.cpp
+++ Source/Engine/Launch/main.cpp
@@ -6,6 +6,22 @@
 #include "Core/Math/Math.h"
 #include "Core/Memory/Allocator.h"
 
+// Just a temp code
+
+#include <chrono>
+
+auto start = std::chrono::high_resolution_clock::now();
+
+void StartProfiling()
+{
+    start = std::chrono::high_resolution_clock::now();
+}
+
+void StopProfiling()
+{
+    printf("%llums\n", (std::chrono::high_resolution_clock::now() - start).count());
+}
+
 int main()
 {
 
