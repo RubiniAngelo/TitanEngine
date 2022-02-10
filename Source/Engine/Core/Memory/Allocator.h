5d996beb15abd942a766e03fd2ae187b65fcfa49
RubiniAngelo <99084249+RubiniAngelo@users.noreply.github.com>
2022-02-10 09:51:19 +0100

    Update Allocator.h

--- Source/Engine/Core/Memory/Allocator.h
+++ Source/Engine/Core/Memory/Allocator.h
@@ -3,12 +3,73 @@
 #include "Core/CoreTypes.h"
 #include "Core/Platform/Memory.h"
 
-typedef int PlayerBase;
-
 /** Default allocator */
 template<typename T>
 class Allocator
 {
 public:
+    FORCEINLINE T* Allocate(const TSize size, const TSize alignment = DEFAULT_ALIGNMENT)
+    {
+        return (T*)Memory::Allocate(size, alignment);
+    }
 
+    FORCEINLINE void Free(T* ptr)
+    {
+        Memory::Free(ptr);
+    }
+};
+
+template<typename T, TSize BlockSize = 2048>
+class StackAllocator
+{
+    struct Block
+    {
+        void* Address;
+        TSize Current;
+        TSize Size;
+
+        Block* Next;
+    };
+    
+public:
+    StackAllocator() : m_Size(BlockSize)
+    {
+        m_Data = (T*)Memory::Allocate(BlockSize, DEFAULT_ALIGNMENT);
+    }
+
+    ~StackAllocator()
+    {
+        Memory::Free(m_Data);
+    }
+
+public:
+    FORCEINLINE T* Allocate(const TSize size, const TSize alignment = DEFAULT_ALIGNMENT)
+    {
+        if(m_Current + size > m_Size)
+        {
+            // If the size is greater than the current block size
+        }
+
+        return nullptr;
+    }
+
+    FORCEINLINE void Free(T* ptr)
+    {
+        /** This type of allocator can't free a single chunk of memory */
+    }
+
+private:
+    FORCEINLINE Block* AllocateBlock(const TSize size)
+    {
+        Block* block    = (Block*)Memory::Allocate(sizeof(Block));
+        block->Address  = Memory::Allocate(size);
+        block->Size     = size;
+        block->Current  = 0;
+
+        return block;
+    }
+
+private:
+    Block*  m_Block;
+    TSize   m_Size;
 };
